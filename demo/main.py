6d4e2e347d9d8d472b334b1ec08dee02af59a94f
nguyencong-dev <nguyenvancong.ou@gmail.com>
2026-02-06 13:59:12 +0700

    feature-test update

--- demo/main.py
+++ demo/main.py
@@ -1,7 +1,7 @@
 import math
 def isPrime(n):
     if n < 2:
-        return False
+       raise ValueError("Invalid Error")
     else:
         for i in range(2, math.isqrt(n)):
             if n % i == 0:
