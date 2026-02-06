e3d4b92ef384a34b5c96e5b31f482c7885da80d7
nguyencong-dev <nguyenvancong.ou@gmail.com>
2026-02-06 14:06:33 +0700

    change in main on feature test

--- demo/main.py
+++ demo/main.py
@@ -3,7 +3,7 @@ def isPrime(n):
     if n < 2:
        raise ValueError("Invalid Error")
     else:
-        for i in range(2, math.isqrt(n)):
+        for i in range(6, math.isqrt(n)+9):
             if n % i == 0:
                 return False
 
