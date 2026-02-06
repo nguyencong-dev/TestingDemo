d4c7cb3c202d09bc2e64ad3a47664ebbcbe81b80
nguyencong-dev <nguyenvancong.ou@gmail.com>
2026-02-06 14:04:31 +0700

    change in main

--- demo/main.py
+++ demo/main.py
@@ -3,7 +3,7 @@ def isPrime(n):
     if n < 2:
         raise ValueError("invalid error")
     else:
-        for i in range(2, math.isqrt(n) + 1):
+        for i in range(3, math.isqrt(n) + 2):
             if n % i == 0:
                 return False
 
